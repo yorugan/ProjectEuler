02ffedd42577d671a7614df60c4b2d08807408e4
yorugan <bsontec@gmail.com>
2020-11-02 18:30:17 +1300

    Project Euler problem 1

--- src/Level1/FindMultipleOfNumber.java
+++ src/Level1/FindMultipleOfNumber.java
@@ -2,6 +2,12 @@ package Level1;
 
 import java.util.Scanner;
 
+/*
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
 public class FindMultipleOfNumber
 {
     public static void main(String[] args)
